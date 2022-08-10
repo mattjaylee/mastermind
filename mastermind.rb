57e70b9174df3a957fbfccef0363aa62fa429921
Matt Lee <matt.jay.lee@gmail.com>
2022-08-10 11:08:22 -0700

    Fix guess checking handling edge cases  -rewrite guess check to handle code key giving yellow instead of green   in instances of multiple of the same color  -code next turn and start game methods for game class

--- mastermind.rb
+++ mastermind.rb
@@ -19,24 +19,44 @@ class Game
     def check_player_guess(secret_code, player_guess)
         code_key = []
         temp_secret_code = secret_code.clone
-        player_guess.each_with_index do |player_color_guess, index|
-            if player_color_guess == temp_secret_code[index]
+        (0..3).each do |i|
+            if player_guess[i] == temp_secret_code[i]
                 code_key.push("Green")
-                temp_secret_code[index] = "GUESSED"
-            elsif temp_secret_code.include? player_color_guess
+                temp_secret_code[i] = nil
+                player_guess[i] = nil
+            end
+        end
+        player_guess.compact!
+        temp_secret_code.compact!
+        player_guess.each do |player_color_guess|
+            if temp_secret_code.include? player_color_guess
                 code_key.push("Yellow")
                 temp_secret_code[temp_secret_code.index(player_color_guess)] = "GUESSED"
             else code_key.push("Gray")
             end
         end
         puts "code key is #{code_key} and code is #{secret_code}"
+        code_key
+    end
+
+    def next_turn(player, secret_code)
+        player_guess = player.guess
+        key = check_player_guess(secret_code, player_guess)
+        unless key == ["Green", "Green", "Green", "Green"]
+            next_turn(player, secret_code)
+        else puts "WINNER YOU GUESSED THE CODE"
+        end
+    end
+
+    def start_game(player)
+        check_player_guess(self.code, player.guess)
     end
 
 end
 
 class Player
 
-    def guess()
+    def guess
         puts "r = red, g = green, b = blue, y = yellow, p = purple, o = orange. Input a combination of 4"
         player_guess = gets.chomp.downcase.split('')
         unless (player_guess - Game::COLORS).empty? && player_guess.length == 4
@@ -44,12 +64,9 @@ class Player
         end
         player_guess
     end
-
 end
 
 
 game = Game.new
 player = Player.new
-guess = player.guess
-#game.check_player_guess(["p", "b", "r", "r"], ["r", "r", "g", "g"])
-game.check_player_guess(game.code, guess)
+game.start_game(player)
